a06d5e861758f8faefedac109c2efdd072cab079
ehrman <riyo0024@gmail.com>
2023-12-19 23:27:00 +0900

    complete modal

---
+++ script.js
@@ -0,0 +1,45 @@
+const buttonOpen = document.getElementById('modalOpen');
+const buttonClose = document.getElementById('modalClose');
+
+const modal = document.getElementById('modal');
+const header = document.querySelector('.header:after');
+const main = document.getElementById('main');
+const footer = document.getElementById('footer');
+
+
+const open = (Elm) =>{
+    Elm.style.display='block';
+    Elm.classList.add("fadeIn");
+    Elm.classList.remove("fadeOut");
+    
+}
+
+const close = (Elm) =>{
+    Elm.classList.add("fadeOut");
+    Elm.classList.remove("fadeIn");
+    //Elm.style.display='none';
+}
+
+var y;
+buttonOpen.addEventListener('click',()=>{
+
+    y=window.scrollY;
+    open(modal);
+    close(main);
+    close(footer);
+    
+});
+
+buttonClose.addEventListener('click',()=>{
+ 
+    close(modal);
+    
+    open(main);
+    open(footer);
+
+    window.scroll({
+        top:y,
+        behavior:"instant"
+    });
+    
+});
