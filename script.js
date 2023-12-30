cacc42765bffa95a04fa6f9459f43c2ec1920340
ehrman <riyo0024@gmail.com>
2023-12-30 16:17:54 +0900

    update product.html script.js

--- script.js
+++ script.js
@@ -67,75 +67,194 @@ modalclick.forEach(function(Elm){
 const selectYear = document.getElementById('year');
 const selectMonth = document.getElementById('month');
 const selectDay = document.getElementById('day');
-const now = new Date();
-const nowYear = now.getFullYear();
-var y = 1910;
-
-while(y<=nowYear){
-    if(y === 1990){
-        selectYear.innerHTML += `<option value="${y}" id="default">${y}</option>`
-    }else{
-        selectYear.innerHTML += `<option value="${y}" >${y}</option>`
-    }
-    y++;
-}
-
-for(var i=1;i<=12;i++){
-    selectMonth.innerHTML += `<option value="${i}">${i}</option>`
-}
-
-for(var i=1;i<=31;i++){
-    selectDay.innerHTML += `<option value="${i}">${i}</option>`
-}
-
-selectYear.addEventListener('focusin',()=>{
-    const def = document.getElementById('default');
-    def.setAttribute('selected','');
-});
-
+const nowYear = new Date().getFullYear();
 
 const thirtyone = ['1','3','5','7','8','10','12'];
 const thirty = ['4','6','9','11'];
 
-
-selectMonth.addEventListener('click',()=>{
-    selectDay.innerHTML = `<option value="" hidden>-</option>`;
-    let year = selectYear.value;
-    let month = selectMonth.value;
-    console.log(month);
-    if(year!=='-' && month!=='-'){
-        let num = 0;
-        if(thirtyone.includes(month)){
-            num = 31;
-        }else if(thirty.includes(month)){
-            num = 30;
-        }else if(year%400 === 0 || (year%100!==0 && year%4==0)){
-            num = 29;
-        }else{
-            num = 28;
+const createList = (l,r,elm,def) =>{
+    if(def===undefined){
+        for(var i=l;i<=r;i++){
+            elm.innerHTML += `<option value="${i}">${i}</option>`
         }
-
-        for(var i=1;i<=num;i++){
-            selectDay.innerHTML += `<option value="${i}">${i}</option>`
+    }else{
+        for(var i=l;i<=r;i++){
+            if(i === def){
+                elm.innerHTML += `<option value="${i}" id="default">${i}</option>`
+            }else{
+                elm.innerHTML += `<option value="${i}" >${i}</option>`
+            }
         }
     }
-})
+}
 
+if(selectYear !== null){
 
+    createList(nowYear-120,nowYear,selectYear,1990);
+    createList(1,12,selectMonth);
+    createList(1,31,selectDay);
 
+    selectYear.addEventListener('focusin',()=>{
+        const def = document.getElementById('default');
+        def.setAttribute('selected','');
+    });
 
+    selectMonth.addEventListener('click',()=>{
+        selectDay.innerHTML = `<option value="" hidden>-</option>`;
+        let year = selectYear.value;
+        let month = selectMonth.value;
+        console.log(month);
+        if(year!=='-' && month!=='-'){
+            let num = 0;
+            if(thirtyone.includes(month)){
+                num = 31;
+            }else if(thirty.includes(month)){
+                num = 30;
+            }else if(year%400 === 0 || (year%100!==0 && year%4==0)){
+                num = 29;
+            }else{
+                num = 28;
+            }
 
-
+            createList(1,num,selectDay);
+        }
+    })
+}
 
 
 /* Select */
 
 const selects = document.querySelectorAll('.select-hidden');
-console.log(selects);
-for(const elm of selects){
-    elm.addEventListener('focusin',()=>{
-        elm.classList.remove('select-hidden');
-    })
+if(selects !== null){
+    selects.forEach((elm)=>{
+        elm.addEventListener('focusin',()=>{
+            elm.classList.remove('select-hidden');
+        });
+    });
 }
+
+
+/* pcs */
+
+const decButton = document.getElementById('pcs-dec');
+const incButton = document.getElementById('pcs-inc');
+const pcsNum = document.getElementById('pcs-input');
+
+if(pcsNum !== null){
+    pcsNum.value = 1;
+    decButton.addEventListener('click',()=>{
+        if(pcsNum.vaule!==0){
+            pcsNum.value--;
+        }
+    });
+
+    incButton.addEventListener('click',()=>{
+        if(pcsNum.value!==99){
+            pcsNum.value++;
+        }
+    });
+}
+
+
+/* Product Img */
+
+const showimages = (img,thumbnails,imagelist) =>{
+    var i=1;
+    imagelist.forEach((image)=>{
+        if(i === 1){
+            img.innerHTML += `<img src="${image}" alt="product-img${i}" class="show">`;
+            thumbnails.innerHTML += `<img src="${image}" alt="product-img${i}" class="show">`;
+        }else{
+            img.innerHTML += `<img src="${image}" alt="product-img${i}" class="">`;
+            thumbnails.innerHTML += `<img src="${image}" alt="product-img${i}" >`;
+        }
+        i++;
+    });
+}
+
+const getImgIndex = (list) =>{
+    const num = list.length;
+    for(var i=0;i<num;i++){
+        if(list[i].classList.contains('show')){
+            return i;
+        }
+    }
+}
+
+
+const productImg = document.getElementById('product-img');
+const productThumbnails = document.getElementById('product-thumb-list');
+const thumbPrev = document.getElementById('thumb-prev');
+const thumbNex = document.getElementById('thumb-nex');
+
+const imagelist1 = [
+    './img/pierce2ver1.jpg',
+    './img/pierce2ver2.jpg',
+    './img/pierce2ver3.jpg',
+    './img/pierce2ver4.jpg',
+]
+
+
+if(productImg !== null){
+    showimages(productImg,productThumbnails,imagelist1);
+
+    const thumbnails = document.querySelectorAll('.product-thumb-list img');
+    const images = document.querySelectorAll('.product-img img');
+    const num = thumbnails.length;
+
+    for(var i=0;i<num;i++){
+        thumbnails[i].addEventListener('click',function(){
+            let nowIndex = getImgIndex(thumbnails);
+            thumbnails[nowIndex].classList.remove('show');
+            images[nowIndex].classList.remove('show');
+
+            this.classList.add('show');
+            nowIndex = getImgIndex(thumbnails);
+            images[nowIndex].classList.add('show');
+        });
+    }
+
+    thumbPrev.addEventListener('click',()=>{
+        let nowIndex =  getImgIndex(thumbnails);
+
+        thumbnails[nowIndex].classList.remove('show');
+        images[nowIndex].classList.remove('show');
+
+        nowIndex+=num-1;
+        nowIndex%=num;
+
+        thumbnails[nowIndex].classList.add('show');
+        images[nowIndex].classList.add('show');
+    });
+
+    thumbNex.addEventListener('click',()=>{
+        let nowIndex =  getImgIndex(thumbnails);
+
+        thumbnails[nowIndex].classList.remove('show');
+        images[nowIndex].classList.remove('show');
+
+        nowIndex++;
+        nowIndex%=num;
+
+        thumbnails[nowIndex].classList.add('show');
+        images[nowIndex].classList.add('show');
+    });
+
+
+    
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
 
 
